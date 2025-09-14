e6ef7dcfe415bbda5f7e48638c50ef5379f51e6e
NextiaIndex <nextiaindex@protonmail.com>
2025-09-14 15:44:24 +0700

    Make dwm using Space Mono as the font
    There is no point of using the nerd font version if my bar
    doesn't have any nerd font glyphs.

--- config.h
+++ config.h
@@ -167,7 +167,7 @@ static void (*bartabmonfns[])(Monitor *) = { NULL /* , customlayoutfn */ };
 #if BAR_PANGO_PATCH
 static const char font[]                 = "monospace 10";
 #else
-static const char *fonts[]               = { "SpaceMono Nerd Font:size=11" };
+static const char *fonts[]               = { "Space Mono:size=11" };
 #endif // BAR_PANGO_PATCH
 static const char dmenufont[]            = "monospace:size=10";
 
